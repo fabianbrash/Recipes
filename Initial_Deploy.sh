a3976798eff7c0116290c63d0360fa3a12c32816
fabianbrash <fabianbrash@gmail.com>
2017-03-05 08:10:11 -0500

    Add files via upload

--- Initial_Deploy.sh
+++ Initial_Deploy.sh
@@ -2,7 +2,7 @@
 
 ##Initial config for a minimal CentOS install
 
-yum upgrade && yum group install "Development Tools" -y && yum install wget ntp vim firewalld epel-release open-vm-tools pciutilssudo  -y
+yum upgrade -y && yum group install "Development Tools" -y && yum install wget ntp vim firewalld epel-release open-vm-tools pciutilssudo  -y
 sleep 15
 systemctl start vmtoolsd && systemctl enable vmtoolsd
 systemctl start ntpd && systemctl enable ntpd
