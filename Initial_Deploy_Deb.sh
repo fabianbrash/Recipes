431417c42fa200870d3ae9750b4267d15089a059
Fabian Brash <fabianbrash@gmail.com>
2017-04-26 12:34:59 -0400

    Update Initial_Deploy_Deb.sh

--- Initial_Deploy_Deb.sh
+++ Initial_Deploy_Deb.sh
@@ -3,9 +3,9 @@
 ##Initial config for Debian based systems i.e Ubuntu
 ##open-vmtools should already be installed with ubuntu server
 
-apt update && apt upgrade
+apt update && apt upgrade -y
 sleep 15
-apt install -y network-manager openssh-server
+apt install network-manager openssh-server -y
 systemctl start NetworkManager && systemctl enable NetworkManager && systemctl start ssh & systemctl enable ssh
 sleep 20
 apt update
