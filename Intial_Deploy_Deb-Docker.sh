66567b2a1c44a8966e559e841395931a56688336
Fabian Brash <fabianbrash@gmail.com>
2017-04-26 12:35:58 -0400

    Update Intial_Deploy_Deb-Docker.sh

--- Intial_Deploy_Deb-Docker.sh
+++ Intial_Deploy_Deb-Docker.sh
@@ -3,19 +3,19 @@
 ##Initial config for Debian based systems i.e Ubuntu and installation of docker-ce
 ##open-vmtools should already be installed with ubuntu server
 
-apt update && apt upgrade
+apt update && apt upgrade -y
 sleep 15
-apt install -y network-manager openssh-server
+apt install network-manager openssh-server -y
 systemctl start NetworkManager && systemctl enable NetworkManager && systemctl start ssh & systemctl enable ssh
 sleep 20
 ##Install AUFS storage driver
-apt install linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
 ##Install all components required for docker-ce
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 ##Now install docker-ce
-apt install docker-ce
+apt install docker-ce -y
 sleep 20
 apt update
