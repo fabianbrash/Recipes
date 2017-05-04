e4e2777dff410d85b1872d0122f173fe4e9dc1ba
Fabian Brash <fabianbrash@gmail.com>
2017-05-04 08:08:56 -0400

    Added Dev Tools Install option

--- Intial_Deploy_Deb-Docker.sh
+++ Intial_Deploy_Deb-Docker.sh
@@ -8,6 +8,8 @@ apt update && apt upgrade -y
 sleep 15
 apt install openssh-server -y
 systemctl start ssh & systemctl enable ssh
+####If you would like to install Development tools uncomment this line
+#apt install build-essential -y
 sleep 20
 ##Install AUFS storage driver
 apt install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
