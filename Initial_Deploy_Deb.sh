ce977a4b1d00dde8db6503f58249f12747da5de2
Fabian Brash <fabianbrash@gmail.com>
2017-05-04 07:52:48 -0400

    network-manager removed

--- Initial_Deploy_Deb.sh
+++ Initial_Deploy_Deb.sh
@@ -2,11 +2,12 @@
 
 ##Initial config for Debian based systems i.e Ubuntu
 ##open-vmtools should already be installed with ubuntu server
+##I am removing the install of network-manager as I found it to be buggy
 
 apt update && apt upgrade -y
 sleep 15
-apt install network-manager openssh-server -y
-systemctl start NetworkManager && systemctl enable NetworkManager && systemctl start ssh & systemctl enable ssh
+apt install openssh-server -y
+systemctl start ssh & systemctl enable ssh
 sleep 20
 apt update
 sleep 15
