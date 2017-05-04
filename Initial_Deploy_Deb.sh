bd880ae6223ed3faa850b2685b568dd206529235
Fabian Brash <fabianbrash@gmail.com>
2017-05-04 08:03:09 -0400

    Added Dev Tools Install

--- Initial_Deploy_Deb.sh
+++ Initial_Deploy_Deb.sh
@@ -8,6 +8,8 @@ apt update && apt upgrade -y
 sleep 15
 apt install openssh-server -y
 systemctl start ssh & systemctl enable ssh
+####If you would like to install Development tools uncomment this line
+#apt install build-essential -y
 sleep 20
 apt update
 sleep 15
