d1a31568d5c18d3130db20a3cbeeb183129f3a26
Fabian Brash <fabianbrash@gmail.com>
2018-01-19 20:47:13 -0500

    Update Initial_Deploy.sh

--- Initial_Deploy.sh
+++ Initial_Deploy.sh
@@ -4,6 +4,7 @@
 
 yum upgrade -y && yum group install "Development Tools" -y && yum install wget ntp vim firewalld epel-release open-vm-tools pciutils tree -y
 sleep 15
+yum install -y openssh-server
 systemctl start vmtoolsd && systemctl enable vmtoolsd
 systemctl start ntpd && systemctl enable ntpd
 firewall-cmd --add-service=ntp --permanent
